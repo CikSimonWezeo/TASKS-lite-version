29f444d67dd1383e57b1aff7cb4e12e56b12f62d
Simon Cik <cik@openlab.sk>
2023-07-04 16:57:35 +0200

    learn-tailwind

---
+++ src/App.vue
@@ -0,0 +1,162 @@
+
+
+<template>
+   <body>
+      <main class="flex h-screen bg-orange-200 overflow-x-scroll"> 
+         <h1 class="font-extrabold">learning tailwind(without vue)</h1>
+         <div class="flex items-start w-screen p-10 overflow-x-scroll">
+            <section class="flex-shrink-0 w-80 mx-4 p-3 bg-blue-300 rounded-lg">
+               <header>
+                  <h3 class="font-bold p-2">FIRST LIST</h3>
+               </header>
+
+               <ul>
+                  <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                  <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                  <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                  <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+               </ul>
+
+               <footer>
+                  <a class="text-gray-700 block hover:bg-blue-200 p-2 rounded-md" href="">
+                     Add a card...
+                  </a>
+               </footer>
+            </section>
+
+            <section class="h-full flex-shrink-0 w-80 mx-4 p-3 bg-blue-300 rounded-lg">
+               <div class="h-full overflow-y-scroll">
+                  <header>
+                        <h3 class="font-bold">SECOND LIST</h3>
+                  </header>
+                  <ul>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer leading-loose">
+                        <span class="font-bold">Leading-loose</span> dolor sit amet consectetur adipisicing elit. Aliquid veniam enim rerum voluptatibus assumenda qui nisi asperiores
+                        reprehenderit tempore necessitatibus autem dolore tempora iste eaque repudiandae, suscipit aliquam molestias quo?
+                     </li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>   
+                  </ul>
+
+                  <footer>
+                     <a class="text-gray-700 block hover:bg-blue-200 p-2 rounded-md" href="">
+                        Add a card...
+                     </a>
+                  </footer>
+               </div>   
+            </section>
+            <section class="flex-shrink-0 w-80 mx-4 p-3 bg-blue-300 rounded-lg">
+                  <header>
+                        <h3 class="font-bold">SECOND LIST</h3>
+                  </header>
+
+                  <ul>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                  </ul>
+
+                  <footer>
+
+                  </footer>
+               </section>
+               <section class="flex-shrink-0 w-80 mx-4 p-3 bg-blue-300 rounded-lg">
+                  <header>
+                        <h3 class="font-bold">SECOND LIST</h3>
+                  </header>
+
+                  <ul>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer">something</li>
+                  </ul>
+
+                  <footer>
+
+                  </footer>
+               </section>
+         </div>
+      </main>
+      <main>
+         <div class="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen">
+            <div v-if="showModal" class="">
+               <div class="text-center">
+                  <textarea v-model="newTask" name="task" id="task" cols="30" rows="10"></textarea>
+                  <button class="text-center" @click="addTask()">Add task</button>
+                  <button class="text-center" @click="showModal = false">Close</button>
+               </div>
+            </div>
+            <div class="p-5">
+               <div class="w-80 mx-4 p-3 bg-blue-300 rounded-lg">
+                  <header>
+                     <h2 class="p2 font-bold ">TASKS</h2>
+                  </header>
+                  
+                  <ul>
+                     <li class="bg-white my-1 p-3 rounded-lg border-b border-gray-400 hover:bg-gray-200 cursor-pointer" v-for="task in tasks">
+                        {{ task.text }}
+                     </li>
+                  </ul>
+
+                  <footer>
+                     <button class="text-gray-700 block hover:bg-blue-200 p-2 rounded-md w-full text-left" @click="showModal=true">Add task...</button>
+                  </footer>
+               </div>
+            </div>
+         </div>
+      </main>
+   </body>
+</template>
+
+<script>
+export default {
+   data(){
+      return{
+         showModal: false,
+         newTask: "",
+         tasks: [],
+      }
+   },
+   methods: {
+      addTask(){
+         this.tasks.push({
+            text: this.newTask,
+         })
+         this.showModal = false
+         this.newTask = ""
+      },
+   },
+}
+</script>
+
